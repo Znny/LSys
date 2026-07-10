bb2859d8c6dbd81007f382040c22bd261cbb4f80
Ryan Cosgrove <designation6of9@gmail.com>
2026-07-09 18:14:02 -0600

    include cstdarg, update linux ifdef

--- src/utility/logging.cpp
+++ src/utility/logging.cpp
@@ -1,34 +1,37 @@
 #include "utility/logging.hpp"
+#include <cstdarg>
 
 //color initialization is currently only needed when running on windows
 #ifdef WIN32
 #include <windows.h>
 static bool bColorsInitialized = false;
+#else
+static bool bColorsInitialized = true;
 #endif
 
 ///logging functions that each severity uses when logging a message
 LoggingFunction_t LoggingFunctions[(int) ELogSeverity::Count]
-        {
-                DefaultEnabledLoggingFunction,
-                DefaultEnabledLoggingFunction,
-                DefaultEnabledLoggingFunction,
-                DefaultEnabledLoggingFunction,
-                DefaultEnabledLoggingFunction,
-                DefaultEnabledLoggingFunction,
-                DefaultEnabledLoggingFunction
-        };
+{
+    DefaultEnabledLoggingFunction,
+    DefaultEnabledLoggingFunction,
+    DefaultEnabledLoggingFunction,
+    DefaultEnabledLoggingFunction,
+    DefaultEnabledLoggingFunction,
+    DefaultEnabledLoggingFunction,
+    DefaultEnabledLoggingFunction
+};
 
 ///files logged to for each severity, defaults to stdout
 FILE* LoggingFiles[(int) ELogSeverity::Count]
-        {
-                stdout,
-                stdout,
-                stdout,
-                stdout,
-                stdout,
-                stdout,
-                stdout
-        };
+{
+    stdout,
+    stdout,
+    stdout,
+    stdout,
+    stdout,
+    stdout,
+    stdout
+};
 
 
 void InitializeLogColors()
@@ -73,26 +76,26 @@ void SetLoggingFunctionForSeverity(const ELogSeverity Severity, LoggingFunction_
 void DefaultEnabledLoggingFunction(FILE* fp, ELogSeverity Severity, const char* fmt, ...)
 {
     const char* SeverityColors[(int) ELogSeverity::Count]
-            {
-                    "\x1b[38;2;255;0;255m",     //fatal
-                    "\x1b[38;2;200;20;200m",    //critical
-                    "\x1b[38;2;255;0;0m",       //error
-                    "\x1b[38;2;255;255;0m",     //warning
-                    "\x1b[38;2;255;255;255m",   //info
-                    "\x1b[38;2;0;200;200m",     //verbose
-                    "\x1b[38;2;0;128;0m",       //debug
-            };
+    {
+        "\x1b[38;2;255;0;255m",     //fatal
+        "\x1b[38;2;200;20;200m",    //critical
+        "\x1b[38;2;255;0;0m",       //error
+        "\x1b[38;2;255;255;0m",     //warning
+        "\x1b[38;2;255;255;255m",   //info
+        "\x1b[38;2;0;200;200m",     //verbose
+        "\x1b[38;2;0;128;0m",       //debug
+    };
 
     const char* SeverityStrings[(int) ELogSeverity::Count]
-            {
-                    "FATAL",
-                    "CRITICAL",
-                    "Error",
-                    "Warning",
-                    "Info",
-                    "Verbose",
-                    "Debug"
-            };
+    {
+        "FATAL",
+        "CRITICAL",
+        "Error",
+        "Warning",
+        "Info",
+        "Verbose",
+        "Debug"
+    };
 
     constexpr int MaxEntryLength = 2048;
     char EntryBuffer[MaxEntryLength] = {0};
